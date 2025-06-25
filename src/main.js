04112028aef13c5e929467026edeb4cf6bd9b373
jangyuhong <jangyu.hong@namutech.co.kr>
2025-06-25 13:47:20 +0900

    로그인 성공 후 토큰 확인 코드 추가

--- src/main.js
+++ src/main.js
@@ -5,12 +5,6 @@ import App from './App.vue'
 import router from './router'
 import Keycloak from 'keycloak-js'
 
-//const app = createApp(App)
-
-//app.use(router)
-
-//app.mount('#app')
-
 const keycloak = new Keycloak({
   url: 'https://keycloak.com:30443',
   realm: 'PAAS',
@@ -21,17 +15,27 @@ keycloak.init({
     onLoad: 'login-required',
     pkceMethod: 'S256',
     checkLoginIframe: false
- }).then(authenticated => {
-  if (authenticated) {
-    console.log('로그인 성공(spero)');
-    const app = createApp(App)
-    app.config.globalProperties.$keycloak = keycloak
-    app.use(router)
-    app.mount('#app')
-  } else {
-    console.log('로그인 실패(spero) 후 keycloak.login() 호출')
-    keycloak.login({
-      redirectUri: window.location.href
-    })
-  }
-})
+ }).then(async authenticated => {
+   if (!authenticated) {
+     console.warn('로그인 실패(spero), 재시도');
+     return keycloak.login({ redirectUri: window.location.href });
+   }
+   
+   console.log('로그인 성공(spero)');
+
+   // 토큰 확인
+   console.log('accessToken:', keycloak.token);
+   console.log('refreshToken:', keycloak.refreshToken);
+
+   const app = createApp(App);
+
+   // 토큰을 글로벌 등록 (optional)
+   app.config.globalProperties.$keycloak = keycloak;
+
+   // Router 사용
+   app.use(router);
+
+   app.mount('#app');
+ }).catch(error => {
+   console.error('Keycloak 초기화 실패:', error);
+ });
