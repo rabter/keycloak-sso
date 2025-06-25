1c1e26005c394608ff3e8327e73e8629006c27b4
jangyuhong <jangyu.hong@namutech.co.kr>
2025-06-25 13:25:53 +0900

    keycloak SSO 연동 테스트 성공

--- src/main.js
+++ src/main.js
@@ -3,9 +3,35 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Keycloak from 'keycloak-js'
 
-const app = createApp(App)
+//const app = createApp(App)
 
-app.use(router)
+//app.use(router)
 
-app.mount('#app')
+//app.mount('#app')
+
+const keycloak = new Keycloak({
+  url: 'https://keycloak.com:30443',
+  realm: 'PAAS',
+  clientId: 'spero',
+})
+
+keycloak.init({ 
+    onLoad: 'login-required',
+    pkceMethod: 'S256',
+    checkLoginIframe: false
+ }).then(authenticated => {
+  if (authenticated) {
+    console.log('로그인 성공(spero)');
+    const app = createApp(App)
+    app.config.globalProperties.$keycloak = keycloak
+    app.use(router)
+    app.mount('#app')
+  } else {
+    console.log('로그인 실패(spero) 후 keycloak.login() 호출')
+    keycloak.login({
+      redirectUri: window.location.href
+    })
+  }
+})
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-cert.pem')),
+    },
+    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'spero.com', 'spero.com:5173'],
+  },
   plugins: [
     vue(),
     vueDevTools(),
